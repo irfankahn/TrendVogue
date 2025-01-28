d31518a3879c399926a0747547d68db046fdf941
Irfanullah Khan <79622197+irfankahn@users.noreply.github.com>
2025-01-27 20:00:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  
+#app.secret_key = 'your_secret_key'  
 
 # Sample product data with reviews
 products = [
